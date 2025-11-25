13f819bf96aeeec22ac7289f9432ca776fd8533a
Suhani-Balooni <suhanibalooni06@gmail.com>
2025-11-25 17:15:05 +0000

    Write a program to take an array arr[] of integers as input, the task is to find the previous greater element for each element of the array in order of their appearance in the array. Previous greater element of an element in the array is the nearest element on the left which is greater than the current element. If there does not exist next greater of current element, then previous greater element for current element is -1.

---
+++ Day51toDay100/Day57/file.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+int n,i,j,found;
+if(scanf("%d",&n)!=1) 
+printf(" 0");
+int arr[n];
+for(i=0;i<n;i++) 
+scanf("%d",&arr[i]);
+for(i=0;i<n;i++) {
+found=0;
+for(j=i-1;j>=0;j--) {
+if(arr[j]>arr[i])
+{
+printf("%d",arr[j]); found=1; break; }
+}
+if(!found)
+printf("-1");
+if(i<n-1)
+printf(", ");
+}
+return 0;
+}
