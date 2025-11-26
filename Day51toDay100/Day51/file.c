4549c9e72bb9ffa4971348258b39b6ea9b9d598d
Suhani-Balooni <suhanibalooni06@gmail.com>
2025-11-26 08:18:38 +0530

    Update file.c

--- Day51toDay100/Day51/file.c
+++ Day51toDay100/Day51/file.c
@@ -4,10 +4,13 @@ int main()
 int n, target, i, first = -1, last = -1;
 scanf("%d", &n);
 int nums[n];
-for(i = 0; i < n; i++) scanf("%d", &nums[i]);
+for(i = 0; i < n; i++)
+scanf("%d", &nums[i]);
 scanf("%d", &target);
-for(i = 0; i < n; i++) if(nums[i] == target) { first = i; break; }
-for(i = n - 1; i >= 0; i--) if(nums[i] == target) { last = i; break; }
+for(i = 0; i < n; i++) 
+if(nums[i] == target) { first = i; break; }
+for(i = n - 1; i >= 0; i--) 
+if(nums[i] == target) { last = i; break; }
 printf("%d %d", first,last);
 return 0;
 }
